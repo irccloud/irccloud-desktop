257d0f4b62cd08bf07bbba801ba77b59ed1d0a9f
Russ Garrett <russ@garrett.co.uk>
2016-03-11 09:59:36 +0000

    Move Windows upgrade logic to a new module

--- app/main.js
+++ app/main.js
@@ -1,39 +1,12 @@
 var app = require('app');
 var path = require('path');
-var child_process = require('child_process');
 var BrowserWindow = require('browser-window');
 var Shell = require('shell');
 var ConfigStore = require('configstore');
 var Menu = require('./menu');
+var SquirrelWindows = require('./squirrel_windows');
 
-var handleStartupEvent = function() {
-  // Handle Squirrel startup events, called by the Windows installer.
-  // https://github.com/electronjs/windows-installer#handling-squirrel-events
-  if (process.platform !== 'win32') {
-    return false;
-  }
-
-  function squirrelUpdater(args, done) {
-    const updateExe = path.resolve(path.dirname(process.execPath), "..", "Update.exe");
-    child_process.spawn(updateExe, args, {detached: true}).on("close", done);
-  }
-
-  const target = path.basename(process.execPath);
-  switch (process.argv[1]) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      squirrelUpdater(['--createShortcut=' + target + ''], app.quit);
-      return true;
-    case '--squirrel-uninstall':
-      squirrelUpdater(['--removeShortcut=' + target + ''], app.quit);
-      return true;
-    case '--squirrel-obsolete':
-      app.quit();
-      return true;
-  }
-};
-
-if (handleStartupEvent()) {
+if (SquirrelWindows.handleStartupEvent()) {
   return;
 }
 
---
+++ app/squirrel_windows.js
@@ -0,0 +1,33 @@
+const app = require('app');
+const child_process = require('child_process');
+const path = require('path');
+
+module.exports = {
+    handleStartupEvent: function() {
+      // Handle Squirrel startup events, called by the Windows installer.
+      // https://github.com/electronjs/windows-installer#handling-squirrel-events
+      if (process.platform !== 'win32') {
+        return false;
+      }
+
+      function squirrelUpdater(args, done) {
+        const updateExe = path.resolve(path.dirname(process.execPath), "..", "Update.exe");
+        child_process.spawn(updateExe, args, {detached: true}).on("close", done);
+      }
+
+      const target = path.basename(process.execPath);
+      switch (process.argv[1]) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+          squirrelUpdater(['--createShortcut=' + target + ''], app.quit);
+          return true;
+        case '--squirrel-uninstall':
+          squirrelUpdater(['--removeShortcut=' + target + ''], app.quit);
+          return true;
+        case '--squirrel-obsolete':
+          app.quit();
+          return true;
+      }
+    }
+}
+
