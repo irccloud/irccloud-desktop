713bf845244403268b9d374e1027523e6d48fcba
Charlie Wolf <charlie@flow180.com>
2016-03-27 15:34:22 -0500

    use path.join for icon

--- app/main.js
+++ app/main.js
@@ -36,7 +36,7 @@ if (shouldQuit) {
 
 function openMainWindow() {
   mainWindow = new BrowserWindow({
-    'icon': __dirname + '/icon.png', 
+    'icon': path.join(__dirname, 'icon.png'), 
     'width': config.get('width'),
     'height': config.get('height'),
     'webPreferences': {
