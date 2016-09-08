426ce197db287fd9ff2d5a68611a4a4be05dd739
James Wheare <james@wheare.org>
2016-09-08 13:05:55 +0100

    Add devtron in dev

--- app/render/preload.js
+++ app/render/preload.js
@@ -1,11 +1,17 @@
 /* Javascript injected into the page on-load */
 var remote = require('electron').remote;
+var is = require('electron-is');
 
 window.IRCCLOUD_DESKTOP_VERSION = remote.app.getVersion();
 
 window.addEventListener('wheel', function (e) {
   // console.log('onwheel');
 });
+
+if (is.dev()) {
+  window.__devtron = {require: require, process: process};
+  require('devtron').install();
+}
 
 require('./focus')();
 require('./zoom')();
