e0a2779be7e39ae32ce5ddd4033b7c86669ae51c
James Wheare <james@wheare.org>
2019-05-16 19:10:47 +0100

    Remove debug logging

--- app/render/notification.js
+++ app/render/notification.js
@@ -2,7 +2,7 @@ var remote = require('electron').remote;
 
 function listenNotification() {
   remote.getCurrentWindow().webContents.executeJavaScript(
-    'new Promise((resolve, reject) => { if (SESSION) { console.log("SESSION avail"); SESSION.on("notificationClick", function () { resolve(); }); } });'
+    'new Promise((resolve, reject) => { if (SESSION) { SESSION.on("notificationClick", function () { resolve(); }); } });'
   ).then(() => {
     remote.app.emit('activate');
     listenNotification();
