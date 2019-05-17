0246737bebc1db4521b728f1d6e07f3d225350bf
James Wheare <james@wheare.org>
2019-05-17 08:53:07 +0100

    Fix memory leak in notificationClick, pointed out by @Macil

--- app/render/user.js
+++ app/render/user.js
@@ -4,7 +4,7 @@ var remote = require('electron').remote;
 function setupUserListener() {
   document.addEventListener("DOMContentLoaded", function (event) {
     remote.getCurrentWindow().webContents.executeJavaScript(
-      'new Promise((resolve, reject) => { if (SESSION) { SESSION.on("init", function () { resolve({ id: this.id, name: this.get("name"), email: this.get("email") }); }); SESSION.on("noAuth", function () { resolve(null); }); } else { resolve(null); } });'
+      'new Promise((resolve, reject) => { if (SESSION) { SESSION.once("init", function () { resolve({ id: this.id, name: this.get("name"), email: this.get("email") }); }); SESSION.once("noAuth", function () { resolve(null); }); } else { resolve(null); } });'
     ).then((result) => {
       ipcRenderer.send('set-user', result);
     });
