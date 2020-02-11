f4f4a0f0a70eaa474ed00b0b9a9b9eba6f9c74f5
James Wheare <james@wheare.org>
2020-02-11 17:13:27 +0000

    travis_wait doesn't seem to work here

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,7 +3,7 @@ set -ev
 yarn run test
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   yarn run snyk
-  travis_wait 20 yarn run mac
+  yarn run mac
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   yarn run linux
