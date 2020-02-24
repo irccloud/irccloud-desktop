c43314319edc8483db16efe4832b115196ae1e0f
James Wheare <james@wheare.org>
2020-02-24 12:14:33 +0000

    Specify edge channel for snap publishing. Install snap publish deps

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,5 +6,8 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   yarn run mac
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  sudo snap install snapcraft --classic
+  sudo snap install multipass --beta --classic
+  sudo snap install review-tools
   yarn run linux
 fi
