6b3a4b242c2c4d68cfe8c69a9aba1b7d9003be6c
James Wheare <james@wheare.org>
2020-05-06 12:41:04 +0100

    Check if we already have a user when calling set-user

--- app/render/user.js
+++ app/render/user.js
@@ -4,7 +4,7 @@ var remote = require('electron').remote;
 function setupUserListener() {
   document.addEventListener("DOMContentLoaded", function (event) {
     remote.getCurrentWindow().webContents.executeJavaScript(
-      'new Promise((resolve, reject) => { if (SESSION) { SESSION.once("init", function () { resolve({ id: this.id, name: this.get("name"), email: this.get("email") }); }); SESSION.once("noAuth", function () { resolve(null); }); } else { resolve(null); } });'
+      'new Promise((resolve, reject) => { if (SESSION) { if (SESSION.id) { resolve({ id: SESSION.id, name: SESSION.get("name"), email: SESSION.get("email") }); } else { SESSION.once("init", function () { resolve({ id: SESSION.id, name: SESSION.get("name"), email: SESSION.get("email") }); }); } SESSION.once("noAuth", function () { resolve(null); }); } else { resolve(null); } });'
     ).then((result) => {
       ipcRenderer.send('set-user', result);
     });
